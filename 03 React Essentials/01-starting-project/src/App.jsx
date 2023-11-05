baa7b585cfbac5c95f15f24327ae938d25f6c7f8
Joshua Latusia <joshuaRMS@users.noreply.github.com>
2023-11-05 01:45:38 +0100

    Added CoreConcept Components to the core-concepts section

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,3 +1,9 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import configImg from './assets/config.png';
+import jsxUiImg from './assets/jsx-ui.png';
+import stateImg from './assets/state-mgmt.png';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core', 'Learn'];
 
 function getRandomInt(max) {
@@ -5,24 +11,52 @@ function getRandomInt(max) {
 }
 
 function Header() {
+  const description = reactDescriptions[getRandomInt(3)];
+
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          {reactDescriptions[getRandomInt(3)]} React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
     </header>
   );
 }
 
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.img} alt="..." />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+          <CoreConcepts title="Components"
+                        description="The core UI building block."
+                        img={componentsImg} />
+          <CoreConcepts title="Config"
+                        description="Configuration is key."
+                        img={configImg}/>
+          <CoreConcepts title="Jsx UI"
+                        description="JSX UI is the hot topic of 2023."
+                        img={jsxUiImg}/>
+          <CoreConcepts title="State MGMT"
+                        description="The state of the art is MGMT."
+                        img={stateImg}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
