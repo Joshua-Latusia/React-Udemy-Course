275dc6ce3c67a736e04c9f98398b901ad8e4803f
Joshua Latusia <joshuaRMS@users.noreply.github.com>
2023-11-06 18:12:28 +0100

    Added data to data.js for the tabcontent bellow tabbuttons

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import { Header } from './components/Header/Header.jsx';
 import { CoreConcept } from './components/CoreConcept/CoreConcept.jsx';
 import { TabButton } from './components/TabButton/TabButton.jsx';
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click on a button');
+  const [ selectedTopic, setSelectedTopic ] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -40,7 +40,18 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {!selectedTopic ? <p>Please select a topic.</p> : null}
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          ) : null}
         </section>
       </main>
     </div>
